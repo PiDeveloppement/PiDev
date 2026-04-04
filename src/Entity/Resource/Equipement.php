1199f6398f3ee1b54783c36209131488b90b9e01
souhail6 <mecherguisouhail8@gmail.com>
2026-04-04 16:30:39 +0100

    creation de la page equipement(crud) et integrer dans la page base.html.twig

---
+++ src/Entity/Resource/Equipement.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity\Resource;
+
+use App\Repository\Resource\EquipementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EquipementRepository::class)]
+class Equipement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $equipement_type = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image_path = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEquipementType(): ?string
+    {
+        return $this->equipement_type;
+    }
+
+    public function setEquipementType(string $equipement_type): static
+    {
+        $this->equipement_type = $equipement_type;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getImagePath(): ?string
+    {
+        return $this->image_path;
+    }
+
+    public function setImagePath(?string $image_path): static
+    {
+        $this->image_path = $image_path;
+
+        return $this;
+    }
+}
